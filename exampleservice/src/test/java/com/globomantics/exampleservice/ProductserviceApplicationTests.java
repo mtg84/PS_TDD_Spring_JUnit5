0c3ed82367765d0ee6f639f65a45903c5a28cfa3
Maria <cuentaxaestudio@gmail.com>
2020-04-01 17:46:50 +0200

    all code

---
+++ exampleservice/src/test/java/com/globomantics/exampleservice/ProductserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.globomantics.exampleservice;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class ProductserviceApplicationTests {
+
+//	@Test
+	public void contextLoads() {
+	}
+
+}
